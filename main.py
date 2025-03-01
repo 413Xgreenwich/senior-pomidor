5759841dacfecb4c4c1a4a00d3b6d9fcc5a02f67
Саня <sanches2101@gmail.com>
2025-03-02 01:58:14 +0300

    аргумент для функции с аннотацией типа

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-print("Hello World!")
+text = "Hello World!"
 
 
 def print_my_text(mytext: str):
-    pass
+    if mytext:
+        print(mytext)
+    else:
+        print("There is no text sadge blah blah blah lorem ipsum")
+
+
+print_my_text(text)
