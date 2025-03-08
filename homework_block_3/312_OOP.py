0260f709b17b26c125469cfe111f3f6012192963
Саня <sanches2101@gmail.com>
2025-03-08 14:54:46 +0300

    3.1.2. OOP homework done

--- homework_block_3/312_OOP.py
+++ homework_block_3/312_OOP.py
@@ -1,24 +1,65 @@
 class BankAccount:
+
     def __init__(self, owner, balance=0):
         self.owner = owner
         self.__balance = balance
+
     def deposit(self, amount):
         if amount > 0:
             self.__balance += amount
-            print() # todo: add info output f-string about deposit
+            print(f"На банковский счёт {self.owner} поступила сумма в размере {amount}")
+
     def withdraw(self, amount):
         if amount <= self.__balance:
             self.__balance -= amount
-            print() # todo add info
+            print(f"С банковского счёта {self.owner} выведена сумма в размере {amount}")
         else:
-            print() # todo add info about error
+            print(
+                f"Неуспешная попытка вывести сумму со счёта {self.owner}: баланс счёта ниже требуемой суммы"
+            )
+
     def get_balance(self):
-        print(self.__balance)
+        print(f"Баланс счёта {self.owner}: {self.__balance}")
 
 
 class SavingsAccount(BankAccount):
-    pass
+    def __init__(self, owner, balance=0, interest_rate=0.05):
+        super().__init__(owner, balance)
+        self.interest_rate = interest_rate
+
+    def deposit(self, amount):
+        if amount > 0:
+            self._BankAccount__balance += amount
+            print(
+                f"На сберегательный счёт {self.owner} поступила сумма в размере {amount}"
+            )
+
+    def apply_interest(self):
+        added_sum = self._BankAccount__balance * self.interest_rate
+        self._BankAccount__balance += added_sum
+        print(
+            f"На счёт {self.owner} начислены проценты на остаток в размере {added_sum}. Сумма на счёте - {self._BankAccount__balance}"
+        )
+
 
 class CheckingAccount(BankAccount):
-    pass
+    def __init__(self, owner, balance=0):
+        super().__init__(owner, balance)
 
+    def deposit(self, amount):
+        if amount > 0:
+            self._BankAccount__balance += amount
+            print(f"На рассчётный счёт {self.owner} поступила сумма в размере {amount}")
+
+    def withdraw(self, amount):
+        self.__balance -= amount
+        print(f"С банковского счёта {self.owner} выведена сумма в размере {amount}")
+
+
+account1 = SavingsAccount("Александр")
+print(account1.owner)
+account1.get_balance()
+account1.deposit(500)
+account1.withdraw(100)
+account1.apply_interest()
+account1.get_balance()
