a9d7ababb9280cd4ab2e227200b981aca0d46316
Саня <sanches2101@gmail.com>
2025-04-03 23:41:52 +0300

    4.2.3 part 1/2

---
+++ homework_block_4_2_3/conftest.py
@@ -0,0 +1,34 @@
+import pytest, requests
+from faker import Faker
+from constants import BASE_URL, AUTH_HEADERS, API_HEADERS, AUTH_DATA
+
+
+fake = Faker()
+
+
+@pytest.fixture(scope="session")
+def auth_session():
+    session = requests.Session()
+    session.headers.update(AUTH_HEADERS)
+
+    response = requests.post(
+        f"{BASE_URL}/login/access-token",
+        headers=AUTH_HEADERS,
+        data=AUTH_DATA,
+    )
+    assert response.status_code == 200, "Ошибка авторизации"
+
+    token = response.json().get("access_token")
+    assert token is not None, "Токен не получен"
+
+    session.headers.update(API_HEADERS)
+    session.headers.update({"authorization": f"Bearer {token}"})
+    return session
+
+
+@pytest.fixture()
+def item_data():
+    return {
+        "title": fake.word().capitalize(),
+        "description": fake.sentence(nb_words=10),
+    }
---
+++ homework_block_4_2_3/constants.py
@@ -0,0 +1,16 @@
+BASE_URL = "https://api.pomidor-stage.ru/api/v1"
+
+AUTH_HEADERS = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Accept": "application/json",
+}
+
+API_HEADERS = {"Content-Type": "application/json", "Accept": "application/json"}
+
+AUTH_DATA = {
+    "username": "sanches2101@gmail.com",
+    "password": "Test8901",
+    "scope": "",
+    "client_id": "",
+    "client_secret": "",
+}
